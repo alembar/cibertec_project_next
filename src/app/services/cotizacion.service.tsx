5758d0e2ad9bfa46799beb245f943f70c00e93e8
Luis Mendoza <luismendoza@MacBook-Pro-de-Luis.local>
2023-08-11 18:40:55 -0500

    Add service CRUD

--- src/app/services/cotizacion.service.tsx
+++ src/app/services/cotizacion.service.tsx
@@ -2,9 +2,26 @@ import axios from 'axios'
 import { handleResponse } from '../providers'
 import { api } from './baseUrl'
 
-const get = (param: string) =>
+const list = (param: string) =>
     axios.get(`${api.cotizacion}?${param}`).then(handleResponse);
 
+const del = (id:number) =>
+    axios.delete(`${api.cotizacion}/${id}`).then(handleResponse)
+
+const create = (data: any) =>
+    axios.post(api.cotizacion, data).then(handleResponse)
+
+const update = (id: number, data: any) =>
+    axios.put(`${api.cotizacion}/${id}`, data).then(handleResponse)
+
+const retrieve = (id: number) =>
+    axios.get(`${api.cotizacion}/${id}`).then(handleResponse)
+
+
 export const cotizacionService = {
-  get
+  list,
+  del,
+  create,
+  retrieve,
+  update
 }
